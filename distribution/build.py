e10f658ce2a3b766c8b5ba2773f415699fbcae5f
Eoin Fennessy <efenness@redhat.com>
2026-05-15 09:42:58 +0100

    chore: remove ibm-cos-sdk

--- distribution/build.py
+++ distribution/build.py
@@ -90,6 +90,7 @@ BASE_REQUIREMENTS = [
     f"ogx=={OGX_VERSION}",
 ]
 
+# TODO(efenness): It's likely that we don't need many of these dependencies - let's see what we can delete
 # Constrain packages we are patching to ensure reliable and repeatable build
 PINNED_DEPENDENCIES = [
     "'kfp-kubernetes==2.14.6'",
@@ -97,8 +98,6 @@ PINNED_DEPENDENCIES = [
     "'botocore==1.35.88'",
     "'boto3==1.35.88'",
     "'aiobotocore==2.16.1'",
-    "'ibm-cos-sdk-core==2.14.2'",
-    "'ibm-cos-sdk==2.14.2'",
     "'setuptools==80.10.2'",
     "'milvus-lite==2.5.1'",
 ]
--- distribution/install-deps.sh
+++ distribution/install-deps.sh
@@ -13,8 +13,6 @@ uv pip install --upgrade \
     'botocore==1.35.88' \
     'boto3==1.35.88' \
     'aiobotocore==2.16.1' \
-    'ibm-cos-sdk-core==2.14.2' \
-    'ibm-cos-sdk==2.14.2' \
     'setuptools==80.10.2' \
     'milvus-lite==2.5.1'
 uv pip install \
